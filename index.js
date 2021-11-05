813e6fbd7f49b7591ea1dbba6fb21b689fa45240
Ansarissab <zahidensari116@gmail.com>
2021-11-05 03:00:21 -0700

    Initial setup with books CRUD

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const lowdb = require("lowdb");
+const PORT = process.env.PORT || 5000;
+
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = lowdb(adapter);
+db.defaults({ books: [] }).write();
+const app = express();
+
+app.db = db;
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ router/books.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.router();
+const { nanoid } = require("nanoid");
+const id_length = 8;
+
+// GET all books
+router.get("/", (req, res) => {
+  const books = req.app.db.get("books");
+  res.status(200).send({ books });
+});
+
+// GET a book with id
+router.get("/:id", (req, res) => {
+  const book = req.app.db.get("books").find({ id: req.params.id });
+
+  if (!book)
+    res.status(404).send({ error: "Couldn't find Book with this id." });
+
+  res.status(200).send({ book });
+});
+
+// POST create a book
+router.post("/", (req, res) => {
+  try {
+    const book = {
+      id: nanoid(id_length),
+      ...req.body,
+    };
+    req.app.db.get("books").push(book).write();
+
+    res.status(201).send({ message: "Book created successfully.", book });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+// UPDATE | PUT update the book with id and
+router.put("/:id", (req, res) => {
+  try {
+    const book = req.app.db
+      .get("books")
+      .find({ id: req.params.id })
+      .assign(req.body)
+      .write();
+
+    if (!book)
+      res.status(404).send({ error: "Couldn't find Book with this id." });
+
+    res.status(200).send({ book });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+// DELETE delete the book with id
+router.delete("/:id", (req, res) => {
+  const book = req.app.db.get("books").remove({ id: req.params.id }).write();
+
+  if (!book)
+    res.status(404).send({ error: "Couldn't find Book with this id." });
+
+  res.status(204).send({ message: "Book deleted successfully." });
+});
