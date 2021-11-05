dce223c92374b2c6e3e490e50ebf5fdfc2dd7e82
Ansarissab <zahidensari116@gmail.com>
2021-11-05 03:03:37 -0700

    Added books router to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const lowdb = require("lowdb");
 const PORT = process.env.PORT || 5000;
 
+const books_router = require("./router/books");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = lowdb(adapter);
@@ -14,5 +15,6 @@ app.db = db;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/books", books_router);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
