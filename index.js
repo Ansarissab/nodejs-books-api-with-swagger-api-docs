a5d0b92c362c628f258c5565554a9535841356f1
Ansarissab <zahidensari116@gmail.com>
2021-11-12 05:05:23 -0700

    Fixed all the endpoints
    Updated CRUD to work properly
    Updated lowdb access to json db

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import { Low, JSONFile } from "lowdb";
 const PORT = process.env.PORT || 5000;
 
 import swaggerUI from "swagger-ui-express";
@@ -22,11 +21,9 @@ const options = {
 };
 
 const specs = swaggerJsDoc(options);
-const db = new Low(new JSONFile("file.json"));
 
 const app = express();
 
-app.db = db;
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use(cors());
 app.use(express.json());
--- router/books.js
+++ router/books.js
@@ -1,32 +1,42 @@
 import express from "express";
 const router = express.Router();
 import { nanoid } from "nanoid";
+import { join, dirname } from "path";
+import { Low, JSONFile } from "lowdb";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, "db.json");
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+await db.read();
+
 const id_length = 8;
 
 // GET all books
 router.get("/", (req, res) => {
-  const books = req.app.db.get("books");
+  const books = db.data.books;
   res.status(200).send({ books });
 });
 
 // GET a book with id
 router.get("/:id", (req, res) => {
-  const book = req.app.db.get("books").find({ id: req.params.id });
+  const book = db.data.books.find((book) => book.id == req.params.id);
 
   if (!book)
     res.status(404).send({ error: "Couldn't find Book with this id." });
-
-  res.status(200).send({ book });
+  else res.status(200).send({ book });
 });
 
 // POST create a book
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const book = {
       id: nanoid(id_length),
       ...req.body,
     };
-    req.app.db.get("books").push(book).write();
+    db.data.books.push(book);
+    await db.write();
 
     res.status(201).send({ message: "Book created successfully.", book });
   } catch (error) {
@@ -35,31 +45,38 @@ router.post("/", (req, res) => {
 });
 
 // UPDATE | PUT update the book with id and
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
-    const book = req.app.db
-      .get("books")
-      .find({ id: req.params.id })
-      .assign(req.body)
-      .write();
+    const book = db.data.books.find((book) => book.id == req.params.id);
 
-    if (!book)
+    if (!book) {
       res.status(404).send({ error: "Couldn't find Book with this id." });
-
-    res.status(200).send({ book });
+    } else {
+      Object.assign(book, req.body);
+      await db.write();
+      res.status(200).send({ book });
+    }
   } catch (error) {
     res.status(500).send({ error: error });
   }
 });
 
 // DELETE delete the book with id
-router.delete("/:id", (req, res) => {
-  const book = req.app.db.get("books").remove({ id: req.params.id }).write();
+router.delete("/:id", async (req, res) => {
+  const book = db.data.books.find((book) => book.id == req.params.id);
+  db.data.books = removeElementFromArray(db.data.books, book);
 
+  await db.write();
   if (!book)
     res.status(404).send({ error: "Couldn't find Book with this id." });
 
   res.status(204).send({ message: "Book deleted successfully." });
 });
+
+const removeElementFromArray = (arr, value) => {
+  return arr.filter((ele) => {
+    return ele.id != value.id;
+  });
+};
 
 export default router;
